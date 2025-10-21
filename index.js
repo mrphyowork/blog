f1a2c68d5bff589af20bae03899571c87d5b289a
Aung Phyo Thu <aungphyothu@Aungs-MacBook-Pro.local>
2025-10-21 13:20:59 +0700

    edited

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ var cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+const validateToken = require("./middleware/validateJwtToken");
+
 const blog = require("./routes/blog");
-app.use("/blog", blog);
+app.use("/blog", validateToken, blog);
 
 const user = require("./routes/user");
 app.use("/user", user);
